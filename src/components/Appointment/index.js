732eac993470735e319d23b88d14092ea3d9810b
Jake Robins <mendahu@gmail.com>
2020-02-18 17:08:50 -0800

    implemented useVisualMode for default state

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,36 @@ import React from "react";
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import Form from "./Form"
 import useVisualMode from "../../hooks/useVisualMode"
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+    )
+
+  const onAdd = () => {
+    transition(CREATE)
+  }
+
   return (
     <article className="appointment" key={props.id}>
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interview={props.interview}/> : <Empty />}
+
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show student={props.interview.student} interview={props.interview}/>
+      )}
+      {mode === CREATE && (
+        <Form interviewers={[]} onCancel={back}/>
+      )}
+
     </article>
   )
 }
