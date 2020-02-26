fe213f9255fdb3e2c83be4c2655a75793efd257e
Jake Robins <mendahu@gmail.com>
2020-02-25 17:37:48 -0800

    form now validates blank interviewer

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -41,23 +41,22 @@ describe("Form", () => {
   });
 
 
-  it("can successfully save after trying to submit an empty student name", () => {
+  it("validates that the interviewer name is not blank", () => {
+    /* 1. Create the mock onSave function */
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
 
-    //Attempts a save with a blank field
-    fireEvent.click(getByText("Save"));
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
+    const { getByText, getByPlaceholderText } = render(<Form interviewers={interviewers} onSave={onSave} />);
     
+    //put in a name
     const input = getByPlaceholderText("Enter Student Name");
-    
     fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+
+    /* 3. Click the save button */
     fireEvent.click(getByText("Save"));
     
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
   });
 
   it("calls onCancel and resets the input field", () => {
